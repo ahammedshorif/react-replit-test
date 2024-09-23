9c216f9191f3ad9f520903a25bd2f007c49c43d3
ahammedshorif <79597517+ahammedshorif@users.noreply.github.com>
2024-09-23 18:38:52 +0000

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter,Router,Route } from 'react-router-dom'
 import Nav from './components/Nav.jsx'
+import Header from './components/Header.jsx'
 function App() {
   return (
     <>
 
       <BrowserRouter>
          <Nav />
+        <Header />
       </BrowserRouter>
 
     </>
